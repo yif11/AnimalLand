ec4de943a02f325e7857005f3722dfd983cef68f
KinohHanten <145346557+KinohHanten@users.noreply.github.com>
2023-09-24 03:01:18 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-var animal = document.getElementById("animal");
+document.addEventListener("DOMContentLoaded", function () {
+  const animal = document.getElementById("animal");
+
   animal.addEventListener("click", function() {
-      requestIdleCallback(() => {
-    const text = document.body.innerHTML;
-    const modifiedText = text.replace(/([。、！？])/g, (match, punctuation) => {
-      const randomAnimalEmoji = "&#x1f" + (1024 + (Math.floor(Math.random() * 64))).toString(16).slice(-3);
-      return randomAnimalEmoji + ' ';
+      requestAnimationFrame(() => {
+        const text = document.body.innerHTML;
+        const modifiedText = text.replace(/([。、！？])/g, (match, punctuation) => {
+          const randomAnimalEmoji = "&#x1f" + (1024 + (Math.floor(Math.random() * 64))).toString(16).slice(-3);
+          return randomAnimalEmoji + ' ';
+        });
+      document.body.innerHTML = modifiedText;
     });
-    document.body.innerHTML = modifiedText;
   });
 });
